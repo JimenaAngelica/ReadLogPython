b555f249566f6b9fef765ae71fcb0299fc647309
JimenaAngelica <mena.angr@gmail.com>
2016-01-23 22:47:01 -0600

    First commit

---
+++ ReadFile.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+#!/usr/bin/python
+import re
+
+def GetSeekCodes():
+    print "Getting seekcodes"
+    p = re.compile(ur'\[EP]\[\d+-\d+]\[\d]\[(\w+)]')
+    line = ReadLineFile()
+    return re.findall(p, line)
+
+def ReadLineFile():
+    print "Reading the file"
+    with open('31122015.log') as f:
+        lines = f.read()
+        return lines
+
+def main():
+    print "HolaMundo!"
+    lista = GetSeekCodes()
+    print len(lista)
+    #Test()
+
+if __name__ == "__main__": main()
